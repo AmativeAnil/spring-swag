23c45fabc3cdfc0bd2d545f3b674a186c325323c
Anil BT <amativeanil@gmail.com>
2018-09-06 23:23:43 +0530

    init

---
+++ src/main/java/com/swag/SpringSwagApplication.java
@@ -0,0 +1,16 @@
+package com.swag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Anil BT
+ *
+ */
+@SpringBootApplication
+public class SpringSwagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwagApplication.class, args);
+	}
+}
---
+++ src/test/java/com/swag/SpringSwagApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swag;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSwagApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
